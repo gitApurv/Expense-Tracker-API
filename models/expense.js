226981a254773419ef0f489a50906c63f272d1ea
gitApurv <apurvmaurya5@gmail.com>
2025-04-22 23:29:21 +0530

    Implemented Expense Model

---
+++ models/expense.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "Food",
+        "Transportation",
+        "Entertainment",
+        "Bills",
+        "Shopping",
+        "Healthcare",
+        "Education",
+        "Other",
+      ],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Expense", expenseSchema);
