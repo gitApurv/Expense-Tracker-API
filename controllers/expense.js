9124186aa436a824714488ac0e26c71abcfdf075
gitApurv <apurvmaurya5@gmail.com>
2025-05-01 02:14:23 +0530

    Added Comments

--- controllers/expense.js
+++ controllers/expense.js
@@ -153,30 +153,42 @@ module.exports.deleteExpense = async (req, res, next) => {
   }
 };
 
+//Get summary of expenses
+//This function will return the total amount spent, the amount spent by category, and the monthly breakdown of expenses
 module.exports.getSummary = async (req, res, next) => {
+  //Getting the user id from the request
+  //This is done by using the middleware in the routes file
   const userId = req.user.id;
 
+  //Finding the expenses
+  //This will return all the expenses of the user
   const expenses = await Expense.find({ user: userId });
 
+  //Checking if the expenses are found
+  //If not, return a 404 error
   if (!expenses) {
     return res.status(404).json({
       message: "No expenses found",
     });
   }
 
+  //Calculating the total amount spent
+  //This is done by using the reduce function on the expenses array
   const totalSpent = expenses.reduce((acc, expense) => {
     return acc + expense.amount;
   }, 0);
-
   const byCategory = {};
   const monthlyBreakdown = {};
 
+  //Calculating the amount spent by category
+  //This is done by using the forEach function on the expenses array
   expenses.forEach(({ amount, category, date }) => {
     byCategory[category] = (byCategory[category] || 0) + amount;
     const month = new Date(date).toISOString().slice(0, 7);
     monthlyBreakdown[month] = (monthlyBreakdown[month] || 0) + amount;
   });
 
+  //Sending the response
   res.status(200).json({
     message: "Summary fetched successfully",
     totalSpent,
